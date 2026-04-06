40e9940a76275a51d4f40d2e846d6ebf5f9f2375
yogaraj <yogaraj.j@simplify3x.com>
2026-04-06 07:50:08 +0530

    Initial commit of Flask application

---
+++ flaskApp.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from huggingface_hub import login
+from pydantic import BaseModel
+
+load_dotenv()
+login(token=os.getenv("HF_TOKEN"))
+
+
+class users(BaseModel):
+    id: int
+    usr_name: str
+    usr_age: int
+
+
+app = FastAPI()
+
+
+@app.post("/users")
+def create_user(user: users):
+    return {"message": "User created successfully"}
+
+
+@app.get("/users/{id}")
+def get_users(id: int):
+    return {"message": "Users fetched successfully", "id": id}
